ad9dbd1111ce557e1fef289481191a45b614913d
Norman Martin <normaneverthonmartin@gmail.com>
2024-10-21 01:51:32 -0500

    Almost finished

--- JavaScript/default.js
+++ JavaScript/default.js
@@ -1,28 +1,100 @@
 //Norman Martin-2300232
 //Tutor:Sirisha Badhika
-const form = document.getElementById('loginForm');
 
-form.addEventListener("submit", function (event) {
-    const name = 'Norman';
-    const password = 'Norman123'
-    const username = document.getElementById('name').value;
-    const userpassword = document.getElementById('password').value;
-    const Message = document.getElementById('Message');
-    event.preventDefault();
+let totalCost=0;
+let totalTax=0;
+const taxRate=.15;
 
-    if (name == username && password == userpassword) {
-        displayMessage("Login is Succesfull", "green")
-        window.location.href = "./webpages/homePage.html";
-    }
-    else {
-        displayMessage("Login is Unsuccesfull", "red")
+
+window.onload = function () {
+
+    const page = document.getElementsByTagName('body')[0].id;
+
+    if (page == 'loginPage') {
+
+        const form = document.getElementById('loginForm');
+
+        form.addEventListener("submit", function (event) {
+            const name = 'Norman';
+            const password = 'Norman123'
+            const username = document.getElementById('name').value;
+            const userpassword = document.getElementById('password').value;
+            const Message = document.getElementById('Message');
+            event.preventDefault();
+
+            if (name == username && password == userpassword) {
+                displayMessage("Login is Succesfull", "green")
+                window.location.href = "./webpages/homePage.html";
+            }
+            else {
+                displayMessage("Login is Unsuccesfull", "red")
+            }
+
+            function displayMessage(message, color) {
+                Message.innerHTML = message;
+                Message.style.color = color;
+            }
+        });
+
     }
 
-    function displayMessage(message, color) {
-        Message.innerHTML = message;
-        Message.style.color = color;
+    if (page == 'servicePage') {
+
+        const Services = document.getElementsByClassName('serviceContent');
+
+        for (let index = 0; index < Services.length; index++) {
+
+            let cost = Services[index].getElementsByClassName('cost');
+            let check = Services[index].getElementsByClassName('checkmark');
+            let tax = Services[index].getElementsByClassName('tax')[0];
+            let subtotal = Services[index].getElementsByClassName('subtotal')[0];
+            let TotalCost=document.getElementById('TotalCost');
+
+            for (let subindex = 0; subindex < check.length; subindex++) {
+
+                check[subindex].addEventListener("click", function (e) {
+                    var sub = parseFloat(subtotal.innerHTML);
+
+
+                    if (isNaN(sub)) {
+                        sub = 0;
+                        var serviceTax = 0;
+                    }
+                    
+                    var serviceCost = parseFloat(cost[subindex].innerHTML.replace('$', ''));
+
+                    if (check[subindex].checked == true) {
+                        sub = sub + serviceCost;
+                        totalCost+=serviceCost;
+                        
+                       
+                    }
+                    else {
+                        sub = sub - serviceCost;
+                        totalCost-=serviceCost;
+
+                    }
+                    serviceTax = (sub * taxRate);
+                    totalTax= (totalCost*taxRate)
+                    tax.innerHTML = serviceTax;
+                    subtotal.innerHTML = sub;
+                    
+                   TotalCost.innerHTML=totalCost+totalTax;
+
+
+
+                });
+
+            }
+               
+
+        }
+
     }
-});
+};
+
+
+
 
 
 
