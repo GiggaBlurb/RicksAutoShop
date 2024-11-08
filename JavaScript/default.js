5c9f6be07d58014051b023afe49258fc550de2ad
Norman Martin <normaneverthonmartin@gmail.com>
2024-11-07 23:29:04 -0500

    Add registration Page

--- JavaScript/default.js
+++ JavaScript/default.js
@@ -10,40 +10,6 @@ window.onload = function () {
 
     const page = document.getElementsByTagName('body')[0].id;
 
-    if (page == 'loginPage') {
-
-        let attemps = 0;
-        const form = document.getElementById('loginForm');
-
-        form.addEventListener("submit", function (event) {
-            const name = 'Norman';
-            const password = 'Norman123'
-            const username = document.getElementById('name').value;
-            const userpassword = document.getElementById('password').value;
-            const Message = document.getElementById('Message');
-            event.preventDefault();
-
-            if (name == username && password == userpassword) {
-                displayMessage("Login is Succesfull", "green")
-                window.location.href = "./webpages/homePage.html";
-            }
-            else {
-                displayMessage("Login is Unsuccesfull", "red")
-                attemps++;
-            }
-
-            if (attemps == 3) {
-                window.location.href = "./webpages/errorPage.html";
-            }
-
-            function displayMessage(message, color) {
-                Message.innerHTML = message;
-                Message.style.color = color;
-            }
-        });
-
-    }
-
     if (page == 'servicePage') {
 
         const Services = document.getElementsByClassName('serviceContent');
