841f6c0c7df5ef313eb2d99a3fbaaa06b37ffec2
Norman Martin <normaneverthonmartin@gmail.com>
2024-11-19 18:05:50 -0500

    Added Gender Frequency Chart

--- JavaScript/stats.js
+++ JavaScript/stats.js
@@ -1,15 +1,51 @@
 
 window.onload = function () {
     const page = document.getElementsByTagName('body')[0].id;
-    if (page === 'checkoutPage') {
+    if (page === 'statspage') {
         ShowUserFrequency();
     }
 };
 
+function ShowUserFrequency() {
+
+    var UsersArray = [];//declare userlist Array
+    if (localStorage.getItem("RegistrationData") !== null) {//if User List is not null
+        UsersArray = JSON.parse(localStorage.getItem("RegistrationData"));//Get User List from local storage
+    }
 
 
-function ShowUserFrequency(){
-    
+    let GenderChart = document.getElementById("GenderChart");
+    let MaxBarHeight = 50;
+
+    const genderdata = [0, 0, 0]
+
+
+    for (let index = 0; index < UsersArray.length; index++) {//iterate list of user
+
+        if (UsersArray[index].gender === "Male") {
+            genderdata[1]++;
+        } if (UsersArray[index].gender === "Female") {
+            genderdata[0]++;
+        } if (UsersArray[index].gender === "Other") {
+            genderdata[2]++;
+        }
+
+    }
+
+    genderdata.forEach(Total => {
+        const bar = document.createElement('div');
+        bar.classList.add('bar');
+        bar.style.height = scale(Total) + "px";
+        bar.innerHTML = Total;
+        GenderChart.append(bar);
+    }
+    )
+
+    function scale(unscaledNum) {
+        return (unscaledNum + 10 / MaxBarHeight) * MaxBarHeight;
+    }
+
+
 
 
 };
