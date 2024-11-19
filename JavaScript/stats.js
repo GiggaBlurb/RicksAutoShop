0f73edad1262eea4eb47e62b3b14e9155e7f2b83
Norman Martin <normaneverthonmartin@gmail.com>
2024-11-19 16:54:45 -0500

    Stats Test Page

---
+++ JavaScript/stats.js
@@ -0,0 +1,15 @@
+
+window.onload = function () {
+    const page = document.getElementsByTagName('body')[0].id;
+    if (page === 'checkoutPage') {
+        ShowUserFrequency();
+    }
+};
+
+
+
+function ShowUserFrequency(){
+    
+
+
+};
